5bd134850472fbabcd4202380aa6fae15aeaca57
hedleyroos <hedleyroos@gmail.com>
2011-09-15 10:22:45 +0200

    Add test to demonstrate incorrect retrieval of non-public tracks for a chart

--- test_settings.py
+++ test_settings.py
@@ -1,11 +1,41 @@
-DATABASE_ENGINE = 'sqlite3'
+import os
 
-INSTALLED_APPS = [
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+ROOT_URLCONF = 'chart.urls'
+
+INSTALLED_APPS = (
+    # jmbo required apps
     'category',
-    'chart',
-    'jmbo',
+    'photologue',
+    'secretballot',
+
+    # elective apps
     'publisher',
-]
+    'jmbo',
+    'category',
+    'music',
+    'chart',
+
+    # django required apps
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
 
 CKEDITOR_MEDIA_PREFIX = '/media/ckeditor/'
 
+SITE_ID = 1
