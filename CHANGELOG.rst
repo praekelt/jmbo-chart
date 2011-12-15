8aaf4975469649579e7c3ce04ce6be8c0478d654
hedleyroos <hedleyroos@gmail.com>
2011-12-15 10:10:10 +0200

    Bump version and update changelog

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,10 @@
 Changelog
 =========
 
+0.0.5 (unreleased)
+------------------
+#. Modify query so non-public tracks for a chart are not returned.
+
 0.0.4 (2011-08-22)
 ------------------
 #. Initial release.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jmbo-chart',
-    version='0.0.4',
+    version='0.0.5 unreleased',
     description='Jmbo chart app.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
